c60b9fe0ca57e32f16bc66961b26c45fc9fcc73b
Oncle Tom <thomas@oncle-tom.net>
2011-03-27 05:10:21 +0200

    * Added getToken method (not working fine yet, problem with frob...)

--- lib/Mindmeister/REST/Auth/getToken.php
+++ lib/Mindmeister/REST/Auth/getToken.php
@@ -1,10 +1,28 @@
 <?php
+/**
+ * Returns the token for a user
+ * 
+ * @see		http://www.mindmeister.com/fr/services/api/explore_method?method=mm.auth.getToken
+ * @author oncletom
+ */
 
 class Mindmeister_REST_Auth_getToken extends Mindmeister_REST_RequestBase
 {
 	const REQUIRES_SIGNING = true;
 	const REQUIRES_AUTHENTICATION = false;
+	const REQUIRES_ACCESS = '';
 
+	/**
+	 * @see lib/Mindmeister/REST/Mindmeister_REST_RequestBase#getMethod()
+	 */
+	public function getMethod()
+	{
+		return 'mm.auth.getToken';
+	}
+	
+	/**
+	 * @see lib/Mindmeister/REST/Mindmeister_REST_RequestBase#initParametersMap()
+	 */
 	public function initParametersMap()
 	{
 		$this->_parametersMap = array(
